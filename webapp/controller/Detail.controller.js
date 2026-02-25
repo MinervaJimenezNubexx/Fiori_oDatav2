3fe92be7c9621c206460dda62223d7ddb4cc3f71
Minerva Jiménez Álvarez <minerva.jimenez@nubexx.es>
2026-02-25 12:06:10 +0000

    feat: details view (not working)

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+    "use strict";
+
+    return BaseController.extend("com.nubexx.hr.odatav2.controller.Detail", {
+        onInit() {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.getRoute("Detail").attachMatched(this.onRouteMatched);
+
+            debugger;
+        },
+
+        onRouteMatched: function (oEvent) {
+            let oParams = oEvent.getParameter("arguments")
+                oDataModel = this.getView().getModel(),
+                sPath = oDataModel.createKey("Employees", {"EmployeeID": oParams.EmployeeID})
+
+            this.getView().bindElement("/" + sPath);
+
+        }
+
+    });
+});
--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -6,17 +6,33 @@ sap.ui.define([
     return Controller.extend("com.nubexx.hr.odatav2.controller.main", {
         onInit() {
             let oDataModel = this.getOwnerComponent().getModel(),
-          oParams = {
-            success: function(oData){
-              debugger;
-            }.bind(this),
-            error: function(oError){
-              debugger;
-            }.bind(this)
-          };
-          
-        oDataModel.read("/Employees", oParams)
+                oParams = {
+                    success: function (oData) {
+                        debugger;
+                    }.bind(this),
+                    error: function (oError) {
+                        debugger;
+                    }.bind(this)
+                };
 
+            /*oDataModel.metadataLoaded().then(function (oEvent) {
+                oKey = oDataModel.createKey("Employees", {
+                    'EmployeeID': '1'
+                });
+
+                oDataModel.read("/" + oKey, oParams);
+            }.bind(this));*/
+
+        },
+
+        onPressItem: function (oEvent) {
+            let oContext = oEvent.getSource().getSelectedItems()[0].getBindingContext(),
+                oItem = oContext.getObject();
+            let oRouter = this.getOwnerComponent().getRouter();
+
+            oRouter.navTo("Detail", { EmployeeID: oItem.EmployeeID });
+
+            //debugger;
         }
     });
 });
